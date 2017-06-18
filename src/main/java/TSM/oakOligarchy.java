435c381d8a1e896028d4a0fa0c54711e3a959a86
steve klein <sjk83@pitt.edu>
2017-06-18 19:50:44 -0400

    added the ability to add player names and select the amount of players

--- src/main/java/TSM/oakOligarchy.java
+++ src/main/java/TSM/oakOligarchy.java
@@ -2,9 +2,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class oakOligarchy{
 	JFrame window = new JFrame("Oakland Oligarchy");
+	//static Information playerInfo;
 	
 	int numberOfPlayers = 0;
 	int turns = 0;
@@ -19,30 +21,37 @@ public class oakOligarchy{
 	Player stephen = new Player("Stephen", 17500);
 		
 	oakOligarchy(){
-		players.add(atta);
-		numberOfPlayers++;
-		players.add(jay);
-		numberOfPlayers++;
-		players.add(anastasia);
-		numberOfPlayers++;
-		players.add(stephen);
-		System.out.println(players);
-		window.setSize(1000, 1000);
+		System.out.println("got here");
+
+		//default size or the JFrame
+		window.setSize(1000, 720);
 		
 		//Find current player and add to menu
-		current = players.get(getIndexOfCurrentPlayer());
-		Menu menu = new Menu(current);
+		//current = players.get(getIndexOfCurrentPlayer());
+		Menu menu = new Menu(new Player("no one",0));
 		window.add(menu, BorderLayout.NORTH);
 		
-		Information playerInfo = new Information(players);
+		Information playerInfo = new Information();
 		window.add(playerInfo, BorderLayout.WEST);
 		
 		GameBoard board = new GameBoard();
 		window.add(board.getBoard(), BorderLayout.CENTER);
 		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		window.setVisible(true);
+		//sleeping while the player info is collected
+		while(playerInfo.playerDataFlag == false){
+			try {
+			   Thread.sleep(50);
+			} catch(InterruptedException e) {
+			}
+		}
+		this.players=playerInfo.players;
+		playerInfo.removeAll();
+		playerInfo.drawPlayerInfo();
+		playerInfo.validate();
+		playerInfo.repaint();
+		System.out.println("got here");
 	}
 	
 	public int getIndexOfCurrentPlayer(){
@@ -54,6 +63,7 @@ public class oakOligarchy{
 	
 	public static void main(String[] args){
 		new oakOligarchy();
+		
 	}
 
 	
